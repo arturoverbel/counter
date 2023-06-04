f1800df703fb20da0410595054bec6a3f7149d9c
Arturo Verbel <arturo-verbel@hotmail.com>
2023-06-04 08:00:44 -0500

    New

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const count = () => {
-    const a = countdown(new Date(2023, 7, 5, 16, 20, 0, 0));
-    document.getElementById("days").innerText = a.days,
-        document.getElementById("hours").innerText = a.hours,
-        document.getElementById("minutes").innerText = a.minutes,
-        document.getElementById("seconds").innerText = a.seconds;
+    const a = countdown(new Date(2023, 6, 5, 16, 20, 0, 0));
+    document.getElementById("months").innerText = a.months;
+    document.getElementById("days").innerText = a.days;
+    document.getElementById("hours").innerText = a.hours;
+    document.getElementById("minutes").innerText = a.minutes;
+    document.getElementById("seconds").innerText = a.seconds;
 
     return a.value;
 
 }
 
-const finish =  () => {
+const finish = () => {
     document.getElementById("headline").innerText = "Tiempo del Amor !!";
     document.getElementById("countdown").style.display = "none";
     document.getElementById("content").style.display = "block";
@@ -18,8 +19,8 @@ const finish =  () => {
 
 (function () {
 
-    if (count() > 0) {finish(); return; }
-    
+    if (count() > 0) { finish(); return; }
+
     x = setInterval(() => {
         if (count() > 0) {
             finish()
